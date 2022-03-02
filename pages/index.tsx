e0e643b543172d9e670d8dd95317cfde261d09cc
andreyscott <andreyscott>
2022-03-02 22:51:58 +0100

    made it work on to the next component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/banner'
 import  Header from '../components/header'
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div  className='max-w-7xl mx-auto'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
           <Header />
+          <Banner />
        
     </div>
   )
