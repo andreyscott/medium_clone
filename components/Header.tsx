07f402a6fd75ab1988425bc60f5ff6244c06365e
andreyscott <andreyscott>
2022-03-07 22:18:03 +0100

    df

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,16 +15,18 @@ const Header = () => {
         <div className="hidden items-center space-x-5 md:inline-flex">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className="rounded-full  px-4 py-1 text-white ">
+          <h3 className="rounded-full border border-black  px-4 py-1  text-black ">
             Follow
           </h3>
         </div>
       </div>
 
-      <div className="flex items-center space-x-5 text-green-600">
+      <div className="flex items-center space-x-5 text-black">
         <h3>Sign In</h3>
-        <h3 className="rounded-full border border-green-600 px-4 py-1">
-          Get Started
+        <h3 className="rounded-full border border-black bg-black px-4 py-1">
+         <span className='text-white'>
+         Get Started
+           </span> 
         </h3>
       </div>
     </header>
